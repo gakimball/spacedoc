a0c192c7eec366363a34cc5ff63b1b93213ed2d0
Geoff Kimball <geoff@geoffkimball.com>
2016-10-02 23:52:35 -0700

    Use "site" option to configure template locals

---
+++ template/index.js
@@ -0,0 +1,10 @@
+/**
+ * Defaults for the template.
+ * @type Object
+ * @prop {String} [title='Site Title'] Site title, displayed in the top bar.
+ * @prop {Object.<String, String>} [nav={}] Top bar navigation. Each key is a URL for a link, and the value is the text for the link.
+ */
+module.exports = {
+  title: 'Site Title',
+  nav: {},
+}

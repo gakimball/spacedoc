73181b1d7ed3f6ffe908cf610b566f5ec014562d
Geoff Kimball <geoff@geoffkimball.com>
2016-10-28 21:49:19 -0700

    Add support for tabbed, multi-language code samples when writing Markdown

--- packages/spacedoc/lib/util/markdown.js
+++ packages/spacedoc/lib/util/markdown.js
@@ -1,10 +1,13 @@
 const hljs = require('highlight.js');
+const markdownIt = require('markdown-it');
+const randomId = require('random-id');
+const stripIndent = require('strip-indent');
 
 /**
  * Default Markdown renderer.
  * @type MarkdownIt
  */
-module.exports = require('markdown-it')({
+const markdown = markdownIt({
   /**
    * Convert a Markdown code fence into an HTML code block.
    * If the language is `html_example`, a code block will be rendered along with a live sample of the HTML.
@@ -18,6 +21,29 @@ module.exports = require('markdown-it')({
       const { value } = hljs.highlight('html', code);
       return `<pre class="sd-codeblock"><code class="hljs ${lang}">${value}</code></pre><div class="sd-codesample">${code}</div>`;
     }
+    // Rendering multiple examples
+    else if (lang === 'multi') {
+      const codeBlocks = [];
+      const id = randomId(6);
+      let tabs = '';
+      let tabContent = '';
+
+      markdownIt({
+        highlight: (code, lang) => {
+          codeBlocks.push({ code, lang, });
+          return '';
+        }
+      }).render(stripIndent(code));
+
+      codeBlocks.map(({ code, lang }, index) => {
+        const { value } = (lang ? hljs.highlight(lang, code) : hljs.highlightAuto(code));
+
+        tabs += `<li class="tabs-title${index > 0 ? '' : ' is-active'}"><a href="#${id+index}"${index > 0 ? '' : 'aria-selected="true"'}>${lang}</a></li>`;
+        tabContent += `<div class="tabs-panel${index > 0 ? '': ' is-active'}" id="${id+index}"><pre class="sd-codeblock"><code class="hljs ${lang}">${value}</code></pre></div>`;
+      }).join('\n');
+
+      return `<pre class="sd-multisample"><ul class="tabs" data-tabs id="${id}">${tabs}</ul><div class="tabs-content" data-tabs-content="${id}">${tabContent}</div></pre>`;
+    }
     // Rendering for everything else
     else {
       const { value } = (lang ? hljs.highlight(lang, code) : hljs.highlightAuto(code));
@@ -25,3 +51,5 @@ module.exports = require('markdown-it')({
     }
   },
 });
+
+module.exports = markdown;
--- test/index.md
+++ test/index.md
@@ -11,3 +11,21 @@ This page pulls test fixtures from every Spacedoc adapter, so we can see how the
 ```html_example
 <button type="button" class="button">Button</button>
 ```
+
+```multi
+    ```js
+    const fs = require('fs');
+
+    module.exports = function load(path) {
+      return fs.readFileSync(path).toString();
+    }
+    ```
+
+    ```ts
+    import fs from 'fs';
+
+    export default function load(path: string): string {
+      return fs.readFileSync(path).toString();
+    }
+    ```
+```

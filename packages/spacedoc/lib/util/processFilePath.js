216d0192e6a0b3e89164f1af7561c55984632b60
Geoff Kimball <geoff@geoffkimball.com>
2016-12-21 19:50:50 -0500

    Simplify logic in processFilePath() util function

--- packages/spacedoc/lib/util/processFilePath.js
+++ packages/spacedoc/lib/util/processFilePath.js
@@ -6,20 +6,17 @@ const replaceExt = require('replace-ext');
  * Process the file path of a documentation page.
  *   - If the file has leading digits (e.g., `01-intro.md`), they will be removed.
  *   - If `newExtension` is passed as a parameter, the file's extension will be changed.
- * @param {String} file - Full file path to process.
+ * @param {String} filePath - Full file path to process.
  * @param {String} [newExtension] - New extension to use.
  * @returns {String} Modified file path.
- * @todo Simplify string processing.
+ * @todo Add pageRoot processing.
  */
-module.exports = function processFilePath(file, newExtension) {
-  const ext = path.extname(file);
-  const base = path.basename(file, ext);
-  const replacement = base.replace(/^\d+-/, '');
-  let newFile = replaceBase(file, replacement);
+module.exports = function processFilePath(filePath, newExtension) {
+  let newPath = replaceBase(filePath, p => p.replace(/^\d+-/, ''));
 
   if (newExtension) {
-    newFile = replaceExt(newFile, `.${newExtension}`);
+    newPath = replaceExt(newPath, `.${newExtension}`);
   }
 
-  return newFile;
+  return newPath;
 }

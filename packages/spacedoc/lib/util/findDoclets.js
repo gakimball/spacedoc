888df290c26550b65d23a161eb3d4811836d031a
Geoff Kimball <geoff@geoffkimball.com>
2017-01-02 14:32:57 -0500

    Move logic for generating the find() Pug helper into its own function

---
+++ packages/spacedoc/lib/util/findDoclets.js
@@ -0,0 +1,48 @@
+const filter = require('lodash.filter');
+const _find = require('lodash.find');
+
+/**
+ * Build a template helper function to find doclets assigned to the page being rendered. The function created is an alias for lodash's `find()` function. The function also filters out private doclets.
+ * @param {Object.<String, Doclet[]>} doclets - Set of doclets to search.
+ * @param {Boolean} [inheritAccess=true] - Filter out an item if any of its ancestors are private.
+ * @returns {PageFindFunction} Find function.
+ */
+module.exports = function findDoclets(doclets = {}, inheritAccess = true) {
+  /**
+   * Find a doclet within a specific adapter's data set.
+   * @callback PageFindFunction
+   * @param {String} scope - Adapter data to search in.
+   * @param {*} predicate - Argument to pass to `lodash.filter`.
+   * @returns {?Object[]} List of doclets, or `undefined` if none were found.
+   * @todo Make visibility of private doclets configurable.
+   */
+  function find(scope, predicate = {}) {
+    if (scope in doclets) {
+      // Uses the passed-in predicate to find an initial list
+      // Then filters out private items
+      return filter(doclets[scope], predicate).filter(doclet => {
+        if (doclet.access === 'private') {
+          return false;
+        }
+        else if (inheritAccess) {
+          for (
+            let item = doclet;
+            typeof item !== 'undefined';
+            item = _find(doclets[scope], ['meta.longname', item.parent])
+          ) {
+            if (item.access === 'private') {
+              return false;
+            }
+          }
+
+          return true;
+        }
+      });
+    }
+    else {
+      return [];
+    }
+  }
+
+  return find;
+}
---
+++ packages/spacedoc/test/findDoclets.js
@@ -0,0 +1,68 @@
+const { expect } = require('chai');
+const findDoclets = require('../lib/util/findDoclets');
+
+describe('findDoclets()', () => {
+  it('returns a function', () => {
+    expect(findDoclets({})).to.be.a('function');
+  });
+
+  describe('find()', () => {
+    let find;
+    const doclets = {
+      // Basic examples
+      one: [
+        { id: 1, kittens: true },
+        { id: 2, kittens: false },
+        { id: 3, kittens: true },
+      ],
+      // Examples for private inheritence
+      two: [
+        {
+          id: 1,
+          meta: {
+            longname: 'one',
+          },
+          access: 'private',
+        },
+        {
+          id: 2,
+          meta: {
+            longname: 'two',
+          },
+          parent: 'one'
+        },
+        {
+          id: 3,
+          meta: {
+            longname: 'three'
+          },
+          parent: 'two',
+        },
+      ],
+    };
+
+    before(() => {
+      find = findDoclets(doclets);
+    });
+
+    it('returns an empty array for a non-existent scope', () => {
+      expect(find('nope')).to.eql([]);
+    });
+
+    it('finds all items within a scope', () => {
+      expect(find('one')).to.eql(doclets.one);
+    });
+
+    it('finds items with certain qualities', () => {
+      expect(find('one', ['kittens', true])).to.have.lengthOf(2);
+    });
+
+    it('filers out private items', () => {
+      expect(find('two', ['id', 1])).to.be.empty;
+    });
+
+    it('filters out items with private ancestors', () => {
+      expect(find('two', ['id', 3])).to.be.empty;
+    });
+  });
+});

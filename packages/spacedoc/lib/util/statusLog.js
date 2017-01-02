f65f90c2871eef0d018c0cde8e7be0b6de75f25c
Geoff Kimball <geoff@geoffkimball.com>
2017-01-01 21:13:38 -0500

    Rename parameters of statusLog() to make their purposes more clear

--- packages/spacedoc/lib/util/statusLog.js
+++ packages/spacedoc/lib/util/statusLog.js
@@ -4,14 +4,14 @@ const { log } = require('gulp-util');
 /**
  * Logs the completion of a page being processed to the console.
  * @private
- * @param {string} file - Name of the file.
- * @param {object} data - Data object associated with the file. The list of adapters is pulled from this.
+ * @param {String} filePath - Original file path.
+ * @param {PageData} page - Page being logged. The list of adapters is pulled from this.
  */
-module.exports = function statusLog(file, data) {
+module.exports = function statusLog(filePath, page) {
   let msg = '';
-  const adapters = Object.keys(data.docs).join(', ');
+  const adapters = Object.keys(page.docs).join(', ');
 
-  msg += `Spacedoc: processed ${cyan(file)}`;
+  msg += `Spacedoc: processed ${cyan(filePath)}`;
 
   if (adapters.length) {
     msg += ` with ${yellow(adapters)}`;
